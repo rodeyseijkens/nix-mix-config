3468b3d3484024b81e1edaa32a6908f867c9e00c
Rodey Seijkens <me@rodey.nl>
2026-07-11 00:07:53 +0200

    feat(hosts): enable ubersicht and herdr on malus

--- hosts/malus/hm.nix
+++ hosts/malus/hm.nix
@@ -1,8 +1,10 @@
 {...}: {
   modules = {
+    ubersicht.enable = true;
     simple-bar.enable = true;
     zen-browser.enable = true;
     google-chrome.enable = true;
+    herdr.enable = true;
 
     vscode.enable = true;
     zed-editor.enable = true;
